518b04c4d3a96820fc7fc1b6a1d0b955cadf3dcc
Andres-Santanaszk <el_chingon94@hotmail.com>
2026-07-29 12:19:50 -0700

    Added ingress, modified frontend Service

--- Terraform/wrkr-nodes.tf
+++ Terraform/wrkr-nodes.tf
@@ -47,9 +47,9 @@ resource "aws_eks_node_group" "general" {
   instance_types = ["t3.small"]
 
   scaling_config {
-    desired_size = 4
-    max_size     = 5
-    min_size     = 3
+    desired_size = 3
+    max_size     = 4
+    min_size     = 2
   }
 
   update_config {
