b8b8c2f1ca54a74631c2cd9e0cd77dfaceb23534
Andres-Santanaszk <el_chingon94@hotmail.com>
2026-07-31 01:36:17 -0700

    Refactor Kubernetes-manifests/, added Ingress Nginx with Helm and automatic certificate generator (ACME).

---
+++ Terraform/ingress-nginx.tf
@@ -0,0 +1,17 @@
+resource "helm_release" "external_nginx" {
+  name = "external"
+
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  namespace        = "ingress"
+  create_namespace = true
+  version          = "4.15.1"
+
+  values = [
+    file("${path.module}/../Kubernetes-manifests/values/nginx-ingress.yaml")
+  ]
+
+  depends_on = [
+    helm_release.aws_lbc
+  ]
+}
